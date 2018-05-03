8f14f8aca48cd1e43f3216b304dc592b102f8726
Brad Thompson <gbradthompson@gmail.com>
2018-05-02 19:04:08 -0600

    update readme, load all 24 files

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ class Config {
   constructor () {
     this.skip_subs_with_commenters_less_than = 1000
     this.skip_users_with_subs_greater_than = 100
-    this.delete_cross_subs_with_overlap_less_than = 10
-    this.number_of_files_to_load = 1 //24
+    this.delete_cross_subs_with_overlap_less_than = 3
+    this.number_of_files_to_load = 24
   }
 }
 module.exports = new Config()
