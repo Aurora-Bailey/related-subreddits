9d14441a3174b1c2af5e3ccf9dace6c9c882b457
Brad Thompson <gbradthompson@gmail.com>
2018-05-12 17:12:44 -0600

    change the way request errors are stored

--- process_subreddit_about.js
+++ process_subreddit_about.js
@@ -15,14 +15,22 @@ class ProcessSubredditAbout {
         this.subreddit_about_pages = JSON.parse(data)
 
         let subreddits_without_data = this.subredditsNotInData(json_output_chain)
+        let subreddits_without_data_length = subreddits_without_data.length
         console.log(JSON.stringify(subreddits_without_data))
-        console.log(subreddits_without_data.length, 'Subreddits found without data!')
+        console.log(subreddits_without_data_length, 'Subreddits found without data!')
         console.log(Object.keys(this.subreddit_about_pages).length, 'Subreddits found on S3.')
         this.pullSubredditData(subreddits_without_data, () => {
-          lib.writeS3BucketGzip('ngau', 'subreddit_about.json', JSON.stringify(this.subreddit_about_pages)).catch(err => {console.error(err)}).then(() => {
+          if (subreddits_without_data_length > 0) {
+            console.log('upload to S3')
+            lib.writeS3BucketGzip('ngau', 'subreddit_about.json', JSON.stringify(this.subreddit_about_pages)).catch(err => {console.error(err)}).then(() => {
+              this.writeAbout(json_output_chain)
+              resolve()
+            })
+          } else {
+            console.log('skip S3 upload')
             this.writeAbout(json_output_chain)
             resolve()
-          })
+          }
         })
       })
 
@@ -36,7 +44,7 @@ class ProcessSubredditAbout {
       json_output_chain[sub].banner_img = this.subreddit_about_pages[sub].banner_img || ''
       json_output_chain[sub].header_img = this.subreddit_about_pages[sub].header_img || ''
       json_output_chain[sub].icon_img = this.subreddit_about_pages[sub].icon_img || ''
-      json_output_chain[sub].description = this.subreddit_about_pages[sub].description_html || this.subreddit_about_pages[subreddit]
+      json_output_chain[sub].description = this.subreddit_about_pages[sub].description_html || this.subreddit_about_pages[sub].error_message
       json_output_chain[sub].subscribers = this.subreddit_about_pages[sub].subscribers || 0
       json_output_chain[sub].accounts_active = this.subreddit_about_pages[sub].accounts_active || 0
       json_output_chain[sub].created_utc = this.subreddit_about_pages[sub].created_utc || 0
@@ -45,7 +53,7 @@ class ProcessSubredditAbout {
       json_output_chain[sub].x_subs.subscribers = []
       json_output_chain[sub].x_subs.over18 = []
       json_output_chain[sub].x_subs.subreddits.forEach(subreddit => {
-        json_output_chain[sub].x_subs.public_description.push(this.subreddit_about_pages[subreddit].public_description_html || this.subreddit_about_pages[subreddit].error)
+        json_output_chain[sub].x_subs.public_description.push(this.subreddit_about_pages[subreddit].public_description_html || this.subreddit_about_pages[subreddit].error_message)
         json_output_chain[sub].x_subs.subscribers.push(this.subreddit_about_pages[subreddit].subscribers || 0)
         json_output_chain[sub].x_subs.over18.push(this.subreddit_about_pages[subreddit].over18 || false)
       })
@@ -57,8 +65,6 @@ class ProcessSubredditAbout {
     Object.keys(json_output_chain).forEach(subreddit => {
       if (subreddit === '_index_subreddits') return false
       if (typeof this.subreddit_about_pages[subreddit] !== 'object') list.push(subreddit)
-      else if (typeof this.subreddit_about_pages[subreddit].id === 'undefined'
-      && typeof typeof this.subreddit_about_pages[subreddit].error === 'undefined') list.push(subreddit)
     })
     return list
   }
@@ -78,7 +84,7 @@ class ProcessSubredditAbout {
       }).catch(err => {
         if (!err.error) err.error = {}
         if (!err.error.reason) err.error.reason = 'unknown'
-        this.subreddit_about_pages[sub] = {error: err.error.reason}
+        this.subreddit_about_pages[sub] = {error_message: err.error.reason}
         console.log('Failed', options.uri, 'set data', JSON.stringify(this.subreddit_about_pages[sub]))
         setTimeout(() => {
           this.pullSubredditData(subreddit_list, callback)
