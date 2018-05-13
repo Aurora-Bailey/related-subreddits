6a2d96bbaf7d0a7046099dd0bb3827010cdfc3e3
Brad Thompson <gbradthompson@gmail.com>
2018-05-12 18:10:40 -0600

    un comment upload script

--- index.js
+++ index.js
@@ -10,18 +10,16 @@ related.start(json_output_chain).catch(err => {console.error(err)}).then(done =>
     console.log('===========', lib.memoryUsed(), lib.totaltime(), '|', 'products done')
     about.start(json_output_chain).catch(err => {console.error(err)}).then(done => {
       console.log('===========', lib.memoryUsed(), lib.totaltime(), '|', 'about done')
-      console.log(JSON.stringify(json_output_chain['philosophy']))
-      return false
-      // lib.createS3Bucket().catch(err => {console.error(err)}).then(bucket => {
-      //   let sent = Object.keys(json_output_chain).length
-      //   Object.keys(json_output_chain).forEach(key => {
-      //     lib.writeS3BucketGzip(bucket, key + '.json', JSON.stringify(json_output_chain[key])).catch(err => {console.error(err)}).then(() => {
-      //       sent--
-      //       console.log(sent)
-      //       if (sent === 0) console.log('done')
-      //     })
-      //   })
-      // })
+      lib.createS3Bucket().catch(err => {console.error(err)}).then(bucket => {
+        let sent = Object.keys(json_output_chain).length
+        Object.keys(json_output_chain).forEach(key => {
+          lib.writeS3BucketGzip(bucket, key + '.json', JSON.stringify(json_output_chain[key])).catch(err => {console.error(err)}).then(() => {
+            sent--
+            console.log(sent)
+            if (sent === 0) console.log('done')
+          })
+        })
+      })
     })
   })
 })
