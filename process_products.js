1c6bd228b8aad5f82beb073d48856d322bd7449e
Brad Thompson <gbradthompson@gmail.com>
2018-05-20 16:08:16 -0600

    load more details about products

--- process_products.js
+++ process_products.js
@@ -28,7 +28,7 @@ class ProcessProducts {
 
   loadParseCSV () {
     return lib.loadDirectoryParsed(this.data_directory, line => {
-      let {author, subreddit, created_utc, retrieved_on, gilded, score, body} = line
+      let {author, subreddit, created_utc, retrieved_on, score, subreddit_id, link_id, parent_id, id, body} = line
 
       // Find Amazon links
       let amazon_product_array = [] // {name: 'Amazon-Product': asin: 'ASDFGHQWER'}
@@ -54,15 +54,35 @@ class ProcessProducts {
         // Products
         if (typeof this.products_object[product.asin] !== 'object') this.products_object[product.asin] = {asin: product.asin, name: product.name, count: 0}
         if (product.name.length > this.products_object[product.asin].name.length) this.products_object[product.asin].name = product.name
-        this.products_object[product.asin].count++
+        this.products_object[product.asin].count++ // count all subreddits
 
         // Subreddits
         if (typeof this.subreddits_object[subreddit] !== 'object') this.subreddits_object[subreddit] = {products: {}}
         if (typeof this.subreddits_object[subreddit].products[product.asin] !== 'object') {
-          this.subreddits_object[subreddit].products[product.asin] = {ref: this.products_object[product.asin], count: 0, first_comment: created_utc, last_comment: created_utc, best_comment: body, comment_score: score}
+          this.subreddits_object[subreddit].products[product.asin] = {
+            ref: this.products_object[product.asin],
+            count: 0,
+            first_comment: created_utc,
+            last_comment: created_utc,
+            best_comment: body,
+            comment_score: score,
+            comment_author: author,
+            comment_id: id,
+            comment_link_id: link_id,
+            comment_created: created_utc,
+            comment_subreddit: subreddit
+          }
         }
-        this.subreddits_object[subreddit].products[product.asin].count++
-        if (score > this.subreddits_object[subreddit].products[product.asin].comment_score) this.subreddits_object[subreddit].products[product.asin].best_comment = body
+        this.subreddits_object[subreddit].products[product.asin].count++ // count per subreddit
+        if (score > this.subreddits_object[subreddit].products[product.asin].comment_score) {
+          this.subreddits_object[subreddit].products[product.asin].best_comment = body
+          this.subreddits_object[subreddit].products[product.asin].comment_score = score
+          this.subreddits_object[subreddit].products[product.asin].comment_author = author
+          this.subreddits_object[subreddit].products[product.asin].comment_id = id
+          this.subreddits_object[subreddit].products[product.asin].comment_link_id = link_id
+          this.subreddits_object[subreddit].products[product.asin].comment_created = created_utc
+          this.subreddits_object[subreddit].products[product.asin].comment_subreddit = subreddit
+        }
         if (created_utc > this.subreddits_object[subreddit].products[product.asin].last_comment) this.subreddits_object[subreddit].products[product.asin].last_comment = created_utc
         if (created_utc < this.subreddits_object[subreddit].products[product.asin].first_comment) this.subreddits_object[subreddit].products[product.asin].first_comment = created_utc
       })
@@ -77,10 +97,14 @@ class ProcessProducts {
       json_output_chain[subreddit].products = {
         asin: [],
         name: [],
-        comment: [],
+        count: [],
         // last_comment: [],
         // first_comment: [],
-        count: []
+        comment: [],
+        score: [],
+        author: [],
+        link: [],
+        created: []
       }
 
       // Flatten to array
@@ -96,14 +120,20 @@ class ProcessProducts {
           s_count: this.subreddits_object[subreddit].products[product].count, // subreddit count
           // last_comment: this.subreddits_object[subreddit].products[product].last_comment,
           // first_comment: this.subreddits_object[subreddit].products[product].first_comment,
-          comment: this.subreddits_object[subreddit].products[product].best_comment
+          comment: this.subreddits_object[subreddit].products[product].best_comment,
+          score: this.subreddits_object[subreddit].products[product].comment_score,
+          author: this.subreddits_object[subreddit].products[product].comment_author,
+          id: this.subreddits_object[subreddit].products[product].comment_id,
+          link_id: this.subreddits_object[subreddit].products[product].comment_link_id,
+          created: this.subreddits_object[subreddit].products[product].comment_created,
+          subreddit: this.subreddits_object[subreddit].products[product].comment_subreddit
         })
       })
 
       // Sort array
       products_array.sort((a, b) => {
-        if (a.s_count == b.s_count) return 0
-        return a.s_count > b.s_count ? -1 : 1
+        if (a.t_count == b.t_count) return 0 // total count
+        return a.t_count > b.t_count ? -1 : 1
       })
 
       // Write top results
@@ -111,10 +141,14 @@ class ProcessProducts {
         num_ads++
         json_output_chain[subreddit].products.asin.push(product.asin)
         json_output_chain[subreddit].products.name.push(product.name)
-        json_output_chain[subreddit].products.count.push(product.s_count)
-        json_output_chain[subreddit].products.comment.push(sanitizeHtml(marked(product.comment)))
+        json_output_chain[subreddit].products.count.push(product.t_count) // total count
         // json_output_chain[subreddit].products.last_comment.push(new Date(product.last_comment * 1000).toGMTString())
         // json_output_chain[subreddit].products.first_comment.push(new Date(product.first_comment * 1000).toGMTString())
+        json_output_chain[subreddit].products.comment.push(sanitizeHtml(marked(product.comment)))
+        json_output_chain[subreddit].products.score.push(product.score)
+        json_output_chain[subreddit].products.author.push(product.author)
+        json_output_chain[subreddit].products.link.push(`https://www.reddit.com/r/${product.subreddit}/comments/${product.link_id.split('_')[1]}/generated-link/${product.id}`)
+        json_output_chain[subreddit].products.created.push(product.created)
       })
     })
     console.log(`${num_ads} ads found`)
